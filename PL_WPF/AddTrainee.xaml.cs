73b1c8d385486ed1e2ce257af81528de4f0c3cb1
Jacobyeger <ajyeger96@gmail.com>
2019-08-04 13:26:09 +0200

    Added Form AddTrainee

---
+++ PL_WPF/AddTrainee.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL_WPF
+{
+    /// <summary>
+    /// Interaction logic for AddTrainee.xaml
+    /// </summary>
+    public partial class AddTrainee : Window
+    {
+        public AddTrainee()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource traineeViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("traineeViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // traineeViewSource.Source = [generic data source]
+        }
+        
+    }
+}
--- PL_WPF/HomePage.xaml.cs
+++ PL_WPF/HomePage.xaml.cs
@@ -23,5 +23,10 @@ namespace PL_WPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
