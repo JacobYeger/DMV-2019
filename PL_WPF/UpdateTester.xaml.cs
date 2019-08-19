4f627b3b271f47d82738c0b1466435983881f9b5
Jacques <Jacques@YAKOV-PC>
2019-08-19 13:12:51 +0300

    updated UpdateTester.xaml.cs

--- PL_WPF/UpdateTester.xaml.cs
+++ PL_WPF/UpdateTester.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,52 +13,157 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using BE;
 using DAL;
+using System.Net.Mail;
+
 namespace PL_WPF
 {
     /// <summary>
-    /// Interaction logic for UpdateTester.xaml
+    /// Interaction logic for AddTester.xaml
     /// </summary>
-    public partial class UpdateTester : Window
+    public partial class AddTester : Window
     {
-        public UpdateTester()
+        public AddTester()
         {
             InitializeComponent();
         }
-
-        private void finish(object sender, RoutedEventArgs e)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Tester tester = new Tester();
-            tester.ID = iDTextBox.Text;
 
+            System.Windows.Data.CollectionViewSource testerViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("testerViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // testerViewSource.Source = [generic data source]
+        }
 
+        private void Finish(object sender, RoutedEventArgs e)
+        {
+            Tester tester = new Tester(); //instantiate a tester 
+            myDAL dal_Imp = new myDAL(); //create instance of dal implementation
+            bool flag = true; //flag to ensure all fields have been filled
 
+            if (iDTextBox.Text.Equals(String.Empty))
+            {
+                MessageBox.Show("Must enter ID number");
+                flag = false;
+            }
+            else
+            {
+                tester.ID = iDTextBox.Text;
+            }
 
+            if (firstNameTextBox.Text.Equals(string.Empty))
+            {
+                MessageBox.Show("Must enter a first name");
+                flag = false;
+            }
             tester.FirstName = firstNameTextBox.Text;
+
+            if (lastNameTextBox.Text.Equals(string.Empty))
+            {
+                MessageBox.Show("Must enter a last name");
+                flag = false;
+            }
             tester.LastName = lastNameTextBox.Text;
+
+            if (birthdayDatePicker.SelectedDate == null)
+            {
+                MessageBox.Show("Must select a birthday");
+                flag = false;
+            }
             tester.Birthday = birthdayDatePicker.SelectedDate.Value.Date;
+
             //email
+            if (EmailTextBox.Text.Equals(string.Empty))
+            {
+                MessageBox.Show("Must enter email");
+                flag = false;
+            }
             MailAddress email = new MailAddress(EmailTextBox.Text);
+
             tester.Email = email;
+
+            if (cityTextBox.Text.Equals(string.Empty))
+            {
+                MessageBox.Show("Must enter city");
+                flag = false;
+            }
             String city = cityTextBox.Text;
+
+            if (streetTextBox.Text.Equals(string.Empty))
+            {
+                MessageBox.Show("Must enter a street");
+                flag = false;
+            }
             String street = streetTextBox.Text;
+
+            if (numberTextBox.Text.Equals(string.Empty))
+            {
+                MessageBox.Show("Must enter number");
+                flag = false;
+            }
             int number = int.Parse(numberTextBox.Text);
+
             tester.Address = new Address
             {
                 Number = number,
                 Street = street,
                 City = city
             };
-            tester.Gender = (Gender)genderComboBox.SelectedItem;
-            tester.PhoneNumber = phoneNumberTextBox.Text;
-            //Trainee-specific stuff
-            tester.VehicleSpecialize = (VehicleType)vehicleSpcializeComboBox.SelectedItem;
-            tester.MAX_TESTS_PER_WEEK = int.Parse(MAX_TESTS_PER_WEEKTextBox.Text);
 
-            //update the tester
-            myDAL md = new myDAL();
-            md.UpdateTester(tester);
-            //Console.WriteLine(tester);
-            this.Close();
+            if (genderComboBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Must select a gender");
+                flag = false;
+            }
+            tester.Gender = (Gender)genderComboBox.SelectedItem;
+
+            if (phoneNumberTextBox.Text.Equals(string.Empty))
+            {
+                MessageBox.Show("Must enter a phone number");
+                flag = false;
+            }
+            tester.PhoneNumber = phoneNumberTextBox.Text;
+
+            //Tester-specific stuff
+            if (vehicleSpcializeComboBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Must slect a vehicle to specialize");
+                flag = false;
+            }
+            tester.VehicleSpecialize = (VehicleType)vehicleSpcializeComboBox.SelectedItem;
+            if (MAX_TESTS_PER_WEEKTextBox.Text.Equals(string.Empty))
+            {
+                MessageBox.Show("Must enter maximum tests per week");
+                flag = false;
+            }
+            tester.MAX_TESTS_PER_WEEK = int.Parse(MAX_TESTS_PER_WEEKTextBox.Text);
+            foreach (var item in Checkboxes.Children)
+            {
+                if (!(item is CheckBox))
+                {
+                    continue;
+                }
+                CheckBox checkbox = item as CheckBox;
+                int i = Grid.GetColumn(checkbox) - 1;
+                int j = Grid.GetRow(checkbox);
+                tester.AvailabilityMatrix[i, j] = (bool)checkbox.IsChecked;
+
+            }
+            //try-catch to send the correct mesage to the user
+            try
+            {
+                dal_Imp.AddTester(tester);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+
+            Console.WriteLine(tester);
+
+            if (flag)
+            {
+                this.Close();
+            }
         }
     }
 }
