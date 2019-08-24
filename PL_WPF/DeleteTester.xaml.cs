7fe0881ac2b20c86644d4b0adf8a742c7d0575aa
helloITdepartment <Jackbenzakein@gmail.com>
2019-08-24 21:26:27 +0300

    Fixed up deleteTester to show a messagebox if the ID entered was invalid or if the testeer doesn't exist in the system and will only close the deleteTester window if it executes correctly

--- PL_WPF/DeleteTester.xaml.cs
+++ PL_WPF/DeleteTester.xaml.cs
@@ -29,12 +29,24 @@ namespace PL_WPF
         private void PromptAuthorization(object sender, RoutedEventArgs e)
         {
             myDAL md = new myDAL();
-            Tester tester = new Tester();
-            tester.ID = DeleteIDTextbox.Text;
-            bool close = md.RemoveTester(tester);
-            if (close)
+            try
             {
-                this.Close();
+                Tester tester = new Tester();
+                tester.ID = DeleteIDTextbox.Text;
+                bool close = md.RemoveTester(tester);
+            
+                if (close)
+                {
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("The tester does not exist in the system");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);    
             }
             List<Tester> Lst = md.GetTesters();
             foreach (Tester tstr in Lst)
